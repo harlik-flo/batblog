3cee6958deb6977923133c6e65fe28d6bf63b393
florent renie <a@agmail.com>
2022-05-19 10:00:20 +0200

    Ajout dernier articles sur la page d'acceuil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Doctrine\Persistence\ManagerRegistry;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,9 +17,20 @@ class MainController extends AbstractController
      * Contrôleur de la page d'accueil
      */
     #[Route('/', name: 'home')]
-    public function home(): Response
+    public function home(ManagerRegistry $doctrine): Response
     {
-        return $this->render('main/home.html.twig');
+        $articleRepo = $doctrine->getRepository(Article::class);
+
+        $articles = $articleRepo->findBy(
+            [], // WHERE du SELECT
+            ['publicationDate' => 'DESC'],  // ORDER BY du SELECT
+            $this->getParameter('app.article.last_article_number_home'), // LIMIT du SELCET (qu'on récupère dans le services.yaml)
+        );
+        dump( $articles);
+
+        return $this->render('main/home.html.twig', [
+            'articles' => $articles,
+        ]);
     }
 
     /*
